c769c78eb2c6c456b16464b496129615a1b20e75
garsteaa <46425783+garsteaa@users.noreply.github.com>
2019-03-22 21:40:55 +0100

    tea

---
+++ Tea-Shop/script.js
@@ -0,0 +1,9 @@
+burger.onclick = function myFunction() {
+    const x = document.querySelector('#mynavigation');
+
+    if(x.className === 'navigation'){
+        x.className = x.className  + ' responsive';
+    } else {
+        x.className = 'navigation';
+    }
+}
