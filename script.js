5e02d62c03d8c530005ace362a7803544df94cde
garsteaa <46425783+garsteaa@users.noreply.github.com>
2019-05-01 17:02:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ burger.onclick = function myFunction() {
     const x = document.querySelector('#mynavigation');
 
     if(x.className === 'navigation'){
-        x.className = x.className  + ' responsive';
+        x.className += ' responsive';
     } else {
         x.className = 'navigation';
     }
